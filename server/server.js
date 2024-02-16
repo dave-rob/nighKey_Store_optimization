54c99eb60ef23e5359cf6558bd2fdc491ace00ed
Dave <dlrobjr@gmail.com>
2024-02-16 10:39:31 -0700

    incorporated redis server for testing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import pg from "pg";
 import dotenv from "dotenv";
-import Redis from "redis";
+import Redis from "ioredis";
 import JSONCache from 'redis-json';
 
 dotenv.config({ path: "../.env" });
@@ -9,7 +9,7 @@ dotenv.config({ path: "../.env" });
 const { PORT, DATABASE_URL, REDIS_URL } = process.env;
 
 const dbRedis = new Redis(REDIS_URL);
-const jsonCache = new JSONCache(dbRedis);
+// const jsonCache = new JSONCache(dbRedis);
 
 const client = new pg.Pool({
   connectionString: DATABASE_URL,
@@ -23,36 +23,37 @@ app.use(express.json());
 
 // let t=performance.now()
 app.get("/api/shoedata", async (req, res) => {
-  // const result = await jsonCache.get("shoes")
-  // if(result === undefined){
+  const result = await dbRedis.get("shoes")
+  // console.log(result);
+  if(result === null){
     // console.log(result);
-    client.query("SELECT id, thumbnails, expandedimg FROM shoedata;").then( (result) => {
-    // await jsonCache.set("shoes", result.rows)
+    client.query("SELECT id, thumbnails, expandedimg FROM shoedata;").then(async (result) => {
+    await dbRedis.set("shoes", JSON.stringify(result.rows))
     res.send(result.rows);
   });
-  // } else {
-    // console.log(result);
-    // res.send(result)
-  // }
+  } else {
+    //  console.log(result);
+    res.send(result)
+  }
   
 });
 // let t1 = performance.now()
 // console.log(`task took ${t1-t} milliseconds`)
 
 
-app.get("/api/shoedata/:id", (req, res) => { 
+app.get("/api/shoedata/:id", async (req, res) => { 
   // let t2 = performance.now()
   const {id} = req.params;
-  // const result =  await dbRedis.get(`shoe${id}`)
-  // if(result === null){
+  const result =  await dbRedis.get(`shoe${id}`)
+  if(result === null){
     client.query("SELECT * FROM shoedata WHERE id = $1;", [id]).then((result) => {
-    // dbRedis.set(`shoe${id}`, JSON.stringify(result.rows[0]))
+    dbRedis.set(`shoe${id}`, JSON.stringify(result.rows[0]))
     res.send(result.rows[0]);
   });
-  // } else{
+  } else{
     // console.log(result)
-    // res.send(result)
-  // }
+    res.send(result)
+  }
 //   let t3 = performance.now()
 // console.log(`task took ${t3-t2} milliseconds`)
 });
